a9607cf8d7f3c61c6da324aafd35691307344baa
chegeni-0608 <mmoa80610@gmail.com>
2024-09-28 15:49:26 +0330

    2024-09-28-RegularExpersionsfrmKeyboardEvents

--- JSON/frmKeyboardEvent.Designer.cs
+++ JSON/frmKeyboardEvent.Designer.cs
@@ -28,8 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.txtNationalCode = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -44,19 +53,86 @@
             // txtNationalCode
             // 
             this.txtNationalCode.Location = new System.Drawing.Point(120, 30);
+            this.txtNationalCode.MaxLength = 10;
             this.txtNationalCode.Name = "txtNationalCode";
             this.txtNationalCode.Size = new System.Drawing.Size(162, 20);
-            this.txtNationalCode.TabIndex = 1;
+            this.txtNationalCode.TabIndex = 0;
+            this.txtNationalCode.TextChanged += new System.EventHandler(this.txtNationalCode_TextChanged);
+            this.txtNationalCode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNationalCode_KeyPress);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "FirstName :";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(120, 74);
+            this.txtFirstName.MaxLength = 100;
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(162, 20);
+            this.txtFirstName.TabIndex = 1;
+            this.txtFirstName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFirstName_KeyPress);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(120, 120);
+            this.txtEmail.MaxLength = 100;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(162, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Mobile :";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(120, 169);
+            this.txtMobile.MaxLength = 11;
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(162, 20);
+            this.txtMobile.TabIndex = 6;
+            this.txtMobile.TextChanged += new System.EventHandler(this.txtMobile_TextChanged);
             // 
             // frmKeyboardEvent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtNationalCode);
             this.Controls.Add(this.label1);
             this.Name = "frmKeyboardEvent";
             this.Text = "frmKeyboardEvent";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,5 +142,12 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtNationalCode;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.Label label4;
     }
 }
--- JSON/frmKeyboardEvent.cs
+++ JSON/frmKeyboardEvent.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,56 @@ namespace JSON
         public frmKeyboardEvent()
         {
             InitializeComponent();
+        }
+
+
+        private void txtNationalCode_KeyPress(object sender, KeyPressEventArgs e)
+        {
+         if(!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                errorProvider1.SetError(txtNationalCode, "فقط عدد وارد کنید");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void txtNationalCode_TextChanged(object sender, EventArgs e)
+        {
+            if(txtNationalCode.TextLength < 10)
+            {
+                errorProvider1.SetError(txtNationalCode, "کد ملی باید ده رقم باشد");
+                txtNationalCode.Focus();
+            }
+        }
+
+
+        private void txtFirstName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+                errorProvider1.SetError(txtFirstName, "فقط حروف وارد کنید");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void txtMobile_TextChanged(object sender, EventArgs e)
+        {
+            Regex regex = new Regex(@"09\d{9}");
+            if(!regex.IsMatch(txtMobile.Text))
+            {
+                errorProvider1.SetError(txtMobile, "فرمت موبایل اشتباه است");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
         }
     }
 }
