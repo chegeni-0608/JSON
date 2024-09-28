a06d4d95ef81f141bfbcfa12c26c52dce1cd4527
chegeni-0608 <mmoa80610@gmail.com>
2024-09-28 13:52:24 +0330

    2024-09-28-AddSerializeDeserializebuttonOnFrom1CreatefrmKeyboardEvents

--- JSON/Form1.Designer.cs
+++ JSON/Form1.Designer.cs
@@ -29,8 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.btnSerializeToJson = new System.Windows.Forms.Button();
             this.txtSerializeOutput = new System.Windows.Forms.TextBox();
+            this.btnSerializeToJson = new System.Windows.Forms.Button();
+            this.btnDeserializeToObject = new System.Windows.Forms.Button();
+            this.btnSerializeToXMl = new System.Windows.Forms.Button();
+            this.btnDeserializeFromXML = new System.Windows.Forms.Button();
+            this.btnKeyboardEvents = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,6 +46,14 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Json Serialize Output :";
             // 
+            // txtSerializeOutput
+            // 
+            this.txtSerializeOutput.Location = new System.Drawing.Point(30, 67);
+            this.txtSerializeOutput.Multiline = true;
+            this.txtSerializeOutput.Name = "txtSerializeOutput";
+            this.txtSerializeOutput.Size = new System.Drawing.Size(437, 263);
+            this.txtSerializeOutput.TabIndex = 2;
+            // 
             // btnSerializeToJson
             // 
             this.btnSerializeToJson.Location = new System.Drawing.Point(30, 336);
@@ -52,19 +64,55 @@
             this.btnSerializeToJson.UseVisualStyleBackColor = true;
             this.btnSerializeToJson.Click += new System.EventHandler(this.btnSerializeToJson_Click);
             // 
-            // txtSerializeOutput
+            // btnDeserializeToObject
             // 
-            this.txtSerializeOutput.Location = new System.Drawing.Point(30, 67);
-            this.txtSerializeOutput.Multiline = true;
-            this.txtSerializeOutput.Name = "txtSerializeOutput";
-            this.txtSerializeOutput.Size = new System.Drawing.Size(437, 263);
-            this.txtSerializeOutput.TabIndex = 2;
+            this.btnDeserializeToObject.Location = new System.Drawing.Point(267, 336);
+            this.btnDeserializeToObject.Name = "btnDeserializeToObject";
+            this.btnDeserializeToObject.Size = new System.Drawing.Size(200, 23);
+            this.btnDeserializeToObject.TabIndex = 3;
+            this.btnDeserializeToObject.Text = "Deserialize To Object";
+            this.btnDeserializeToObject.UseVisualStyleBackColor = true;
+            this.btnDeserializeToObject.Click += new System.EventHandler(this.btnDeserializeToObject_Click);
+            // 
+            // btnSerializeToXMl
+            // 
+            this.btnSerializeToXMl.Location = new System.Drawing.Point(30, 375);
+            this.btnSerializeToXMl.Name = "btnSerializeToXMl";
+            this.btnSerializeToXMl.Size = new System.Drawing.Size(200, 23);
+            this.btnSerializeToXMl.TabIndex = 4;
+            this.btnSerializeToXMl.Text = "Serialize To XML";
+            this.btnSerializeToXMl.UseVisualStyleBackColor = true;
+            this.btnSerializeToXMl.Click += new System.EventHandler(this.btnSerializeToXMl_Click);
+            // 
+            // btnDeserializeFromXML
+            // 
+            this.btnDeserializeFromXML.Location = new System.Drawing.Point(267, 375);
+            this.btnDeserializeFromXML.Name = "btnDeserializeFromXML";
+            this.btnDeserializeFromXML.Size = new System.Drawing.Size(200, 23);
+            this.btnDeserializeFromXML.TabIndex = 5;
+            this.btnDeserializeFromXML.Text = "Deserialize XAM To  Object";
+            this.btnDeserializeFromXML.UseVisualStyleBackColor = true;
+            this.btnDeserializeFromXML.Click += new System.EventHandler(this.btnDeserializeFromXML_Click);
+            // 
+            // btnKeyboardEvents
+            // 
+            this.btnKeyboardEvents.Location = new System.Drawing.Point(30, 415);
+            this.btnKeyboardEvents.Name = "btnKeyboardEvents";
+            this.btnKeyboardEvents.Size = new System.Drawing.Size(200, 23);
+            this.btnKeyboardEvents.TabIndex = 6;
+            this.btnKeyboardEvents.Text = "Keyboard Events";
+            this.btnKeyboardEvents.UseVisualStyleBackColor = true;
+            this.btnKeyboardEvents.Click += new System.EventHandler(this.btnKeyboardEvents_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKeyboardEvents);
+            this.Controls.Add(this.btnDeserializeFromXML);
+            this.Controls.Add(this.btnSerializeToXMl);
+            this.Controls.Add(this.btnDeserializeToObject);
             this.Controls.Add(this.txtSerializeOutput);
             this.Controls.Add(this.btnSerializeToJson);
             this.Controls.Add(this.label1);
@@ -78,8 +126,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnSerializeToJson;
         private System.Windows.Forms.TextBox txtSerializeOutput;
+        private System.Windows.Forms.Button btnSerializeToJson;
+        private System.Windows.Forms.Button btnDeserializeToObject;
+        private System.Windows.Forms.Button btnSerializeToXMl;
+        private System.Windows.Forms.Button btnDeserializeFromXML;
+        private System.Windows.Forms.Button btnKeyboardEvents;
     }
 }
 
--- JSON/Form1.cs
+++ JSON/Form1.cs
@@ -1,13 +1,17 @@
 ﻿using JSON.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace JSON
 {
@@ -17,10 +21,8 @@ namespace JSON
         {
             InitializeComponent();
         }
+           List<EmploeeModel> _emplooeList = new List<EmploeeModel> { 
 
-        private void btnSerializeToJson_Click(object sender, EventArgs e)
-        {
-            var emploeeList = new List<EmploeeModel> { 
             new EmploeeModel {Id = 101, FirstName = "Morteza", LastName = "Chegeni"},
             new EmploeeModel {Id = 101, FirstName = "Morteza", LastName = "Chegeni"},
             new EmploeeModel {Id = 101, FirstName = "Morteza", LastName = "Chegeni"},
@@ -28,9 +30,57 @@ namespace JSON
             new EmploeeModel {Id = 101, FirstName = "Morteza", LastName = "Chegeni"},
             new EmploeeModel {Id = 101, FirstName = "Morteza", LastName = "Chegeni"},
             };
-
-           string json = Newtonsoft.Json.JsonConvert.SerializeObject(emploeeList);
+        private void btnSerializeToJson_Click(object sender, EventArgs e)
+        {
+           string json = Newtonsoft.Json.JsonConvert.SerializeObject(_emplooeList);
+            var filePath = Application.StartupPath + "\\" + "data.json";
+            File.WriteAllText(filePath, json);  
             txtSerializeOutput.Text = json;
+        }
+
+        private void btnDeserializeToObject_Click(object sender, EventArgs e)
+        {
+            var filePath = Application.StartupPath + "\\" + "data.json";
+            var data = File.ReadAllText(filePath);
+            List<EmploeeModel> emploeeList = JsonConvert.DeserializeObject<List<EmploeeModel>>(data);
+        }
+
+        private void btnSerializeToXMl_Click(object sender, EventArgs e)
+        {
+            XmlSerializer xsSubmit = new XmlSerializer(typeof(List<EmploeeModel>));
+            var xml = "";
+
+            using (var sww = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sww))
+                {
+                    xsSubmit.Serialize(writer, _emplooeList);
+                    xml = sww.ToString(); // Your XML
+                }
+            }
+
+            var filePath = Application.StartupPath + "\\" + "data.xml";
+            File.WriteAllText(filePath, xml);
+            txtSerializeOutput.Text = xml;
+        }
+
+        private void btnDeserializeFromXML_Click(object sender, EventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<EmploeeModel>));
+
+            var filePath = Application.StartupPath + "\\" + "data.xml";
+            StreamReader reader = new StreamReader(filePath);
+            var result = (List<EmploeeModel>)serializer.Deserialize(reader);
+
+            reader.Close(); 
+          
+
+        }
+
+        private void btnKeyboardEvents_Click(object sender, EventArgs e)
+        {
+            var frm = new frmKeyboardEvent();
+            frm.ShowDialog();
         }
     }
 }
